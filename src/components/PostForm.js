1c407447c8d5aed92118525c8ca3da16256a992e
MayBlackwood <ksenia.bobtsova@gmail.com>
2020-11-05 16:13:06 +0300

    Change form component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,20 +3,38 @@ import React, { Component } from 'react';
 class PostForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            title: "",
+        };
     }
 
     handleSubmitClick = (e) => {
         e.preventDefault();
+        console.log(this.state);
+    }
+
+    handleInputChange = (e) => {
+        this.setState(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
     }
 
     render () {
+        const { title } = this.state;
         return (
             <form onSubmit={this.handleSubmitClick}>
                 <h1>POST FORM</h1>
                 <div className="form-group">
                     <label htmlFor="title">Post's Title</label>
-                    <input type="text" className="form-control" id="title"></input>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        value={title}
+                        name="title"
+                        onChange={this.handleInputChange}
+                    />
                 </div>
                 <button className="btn btn-success" type="submit">Create Post</button>
             </form>
